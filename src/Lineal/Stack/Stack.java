8eb76b52ef9e47df884d04bd9832d811612c4074
Luca Tocce <luca.tocce1@gmail.com>
2024-11-29 11:46:51 -0300

    node class created

---
+++ src/Lineal/Stack/Stack.java
@@ -0,0 +1,11 @@
+package Lineal.Stack;
+
+public class Stack {
+    //atributes
+
+    //reference to top node, which represent the higher element (last putted in stack)
+    private Node top;
+
+    //methods
+
+}
